5574ca3c406a9b1552708f24d55734e49ecc9acc
byelyanskyy-glitch <byelyanskyy@gmail.com>
2026-06-22 13:40:14 +0200

    main.py

--- main.py
+++ main.py
@@ -54,3 +54,6 @@ async def bot_webhook(request: Request):
         })
         
     return {"status": "ok"}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=10000)
